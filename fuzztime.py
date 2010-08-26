3c86fa1515731e66b2096c544ba3871e4c7e38d4
Corentin Delcourt <codl@aquageek.net>
2010-08-26 20:42:02 +0200

    O.9

--- fuzztime.py
+++ fuzztime.py
@@ -2,13 +2,6 @@
 
 import time
 
-# SUPER SECRET EASTER EGG
-partypartyparty=True
-
-t=time.localtime(time.time())
-h=t[4]
-m=t[5]
-
 # Words
 hours = ( 
     "twelve",
@@ -25,24 +18,41 @@ hours = (
     "eleven"
     )
 
-minutes = (
-    0 : "% o'clock"
-    1 : "past %"
-    10 : "ten past %"
-    15 : "quarter past %"
-    20 : "twenty past %"
-    30 : "half past %"
-    40 : "twenty to !"
-    45 : "quarter to !"
-    50 : "ten to !"
-    60 : "!"
-    )
+minutes = { # % will be replaced by the current hour, ! will be replaced by the next hour
+    0: "% o'clock",
+    1: "past %",
+    10: "ten past %",
+    15: "quarter past %",
+    20: "twenty past %",
+    30: "half past %",
+    40: "twenty to !",
+    45: "quarter to !",
+    50: "ten to !",
+    60: "!"
+    }
 
+almost = "almost %"
 exactly = "exactly %"
 
+
 def fuzzy(h,m):
+    diff = 60
     for i in minutes.iterkeys():
-      #Do stuff
+        tmpdiff=m-i
+        if abs(tmpdiff) < abs(diff):
+            diff=tmpdiff
+            ret=minutes[i].replace("%", hours[h]).replace("!", hours[h+1])
+    if diff == 0:
+        ret=exactly.replace("%", ret)
+    elif diff >= -2:
+        ret=almost.replace("%", ret)
+    if partypartyparty and h == 0 and m == 0:
+        ret="PARTYPARTYPARTY"
+    return ret
 
 if __name__ == "__main__":
-    print fuzzy(h,m)
+    t=time.localtime(time.time())
+    # SUPER SECRET EASTER EGG
+    partypartyparty=True
+    # I WONDER WHAT IT DOES
+    print fuzzy(t[3]%12,t[4])
